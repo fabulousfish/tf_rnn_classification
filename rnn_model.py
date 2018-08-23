8b61bf2cad75ef89ced0838de50af82a69e62688
xinyukong <xykong@bupt.edu.cn>
2018-08-23 15:23:37 +0800

    query_9_classification

--- rnn_model.py
+++ rnn_model.py
@@ -8,21 +8,21 @@ class TRNNConfig(object):
 
     # 模型参数
     embedding_dim = 64      # 词向量维度
-    seq_length = 20        # 序列长度
-    num_classes = 9        # 类别数
+    seq_length = 600        # 序列长度
+    num_classes = 10        # 类别数
     vocab_size = 5000       # 词汇表达小
 
     num_layers= 2           # 隐藏层层数
     hidden_dim = 128        # 隐藏层神经元
     rnn = 'lstm'             # lstm 或 gru
 
-    dropout_keep_prob = 0.9 # dropout保留比例
-    learning_rate = 0.01    # 学习率
+    dropout_keep_prob = 0.8 # dropout保留比例
+    learning_rate = 1e-3    # 学习率
 
-    batch_size = 20         # 每批训练大小
-    num_epochs = 100          # 总迭代轮次
+    batch_size = 128         # 每批训练大小
+    num_epochs = 10          # 总迭代轮次
 
-    print_per_batch = 20    # 每多少轮输出一次结果
+    print_per_batch = 100    # 每多少轮输出一次结果
     save_per_batch = 10      # 每多少轮存入tensorboard
 
 
